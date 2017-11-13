c66b2c15c2ed384656ff98c5072dcf62e51f83a6
Vadim Proga Krylov <progavs@gmail.com>
2017-11-13 23:35:09 +0500

    first commit

---
+++ Lecture4HomeWork/Lecture4HomeworkClasses.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture4HomeWorkClasses
+{
+    public class Task1
+    {
+        private int[] arr { get; set; }
+
+        public Task1()
+        {
+            arr = new int[20];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                arr[i] = rnd.Next(-10000, 10000);
+            }
+        }
+
+        public int[] GetArray()
+        {
+            return arr;
+        }
+
+        public int FindPairValuesMod3()
+        {
+            int pairCount = 0;
+
+            for (int i = 0; i < 19; i++)
+            {
+                if (arr[i] % 3 == 0 && arr[i + 1] % 3 == 0)
+                {
+                    pairCount++;
+                }
+            }
+            return pairCount;
+        }
+
+    }
+
+    public class Task2
+    {
+        private int[] arr;
+        private int arrLength;
+        private int arrStartValue;
+        private int arrValueStep;
+
+        private Task2()
+        { }
+
+        public Task2(int arrLength, int arrStartValue, int arrValueStep)
+        {
+            this.arrLength = arrLength;
+            this.arrValueStep = arrValueStep;
+            this.arrStartValue = arrStartValue;
+
+            this.arr = new int[this.arrLength];
+            this.arr[0] = this.arrStartValue;
+
+            for (int i = 1; i < this.arrLength; i++)
+            { 
+                this.arrStartValue += this.arrValueStep;
+                this.arr[i] = this.arrValueStep;
+            }
+        }
+
+        public Task2(string inputFilePath)
+        {
+            List<string> inputArr = File.ReadAllLines(inputFilePath).ToList();
+            this.arr = new int[inputArr.Count];
+            this.arrLength = inputArr.Count;
+
+            for (int i = 0; i < inputArr.Count; i++)
+            {
+                this.arr[i] = int.Parse(inputArr[i]);
+            }
+        }
+
+        public int[] GetArray()
+        {
+            return arr;
+        }
+
+        public string MaxCount()
+        {
+            this.arr = this.arr.OrderByDescending(x => x).ToArray();
+            int maxVal = arr[0];
+            int countMaxVal = 0;
+
+            for (int i = 1; i < this.arrLength; i++)
+            {
+                if (maxVal == arr[i])
+                {
+                    countMaxVal++;
+                }
+            }
+
+            string outputStr = "Максимальное значение в массиве: " + maxVal + " и встречается в количестве: " + countMaxVal + " раз.";
+            return outputStr;
+        }
+
+        public int Summ()
+        {
+            int summ = 0;
+
+            for (int i = 0; i < this.arrLength; i++)
+            {
+                summ += this.arr[i];
+            }
+
+            return summ;
+        }
+
+        public void Inverse()
+        {
+            for (int i = 0; i < this.arrLength; i++)
+            {
+                this.arr[i] *= -1;
+            }
+        }
+
+        public void Multi(int value)
+        {
+            for (int i = 0; i < this.arrLength; i++)
+            {
+                this.arr[i] *= value;
+            }
+        }
+
+        public void OutputToFile(string outputFilePath)
+        {
+            string[] strArr = new string[this.arrLength];
+
+            for (int i = 0; i < this.arrLength; i++)
+            {
+                strArr[i] = this.arr[i].ToString();
+            }
+
+            File.WriteAllLines(outputFilePath, strArr);
+        }
+
+    }
+
+    public static class Task3
+    {
+        /// <summary>
+        /// Метод проверки введенных данных пользователя на корректность.
+        /// </summary>
+        /// <param name="login">Логин пользователя</param>
+        /// <param name="password">Пароль пользователя</param>
+        /// <returns></returns>
+        /// 
+
+        private static string[,] logPassArray;
+        private static string inputLogPassTextFilePath = @"C:\logPass.txt";
+        private static int arrLength;
+
+        static Task3()
+        {
+            string[] strArr = File.ReadAllLines(inputLogPassTextFilePath);
+            logPassArray = new string[2, strArr.Length];
+            arrLength = strArr.Length;
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                string[] tempArr = strArr[i].Split(' ').Where(x => x != "").ToArray();
+                logPassArray[0, i] = tempArr[0]; //Логин
+                logPassArray[1, i] = tempArr[1]; //Ппроль
+            }
+        }
+
+        private static bool Validation(string login, string password)
+        {
+            bool valid = false;
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                if (login.Equals(logPassArray[0, i]) && password.Equals(logPassArray[1, i]))
+                {
+                    valid = true;
+                }
+            }
+            return valid;
+        }
+
+        /// <summary>
+        /// Метод предоставляющий интерфейс для ввода данных пользователя (логина и пароля)
+        /// </summary>
+        public static void LoginPasswordValidation()
+        {
+            int i = 0;
+            bool result = false;
+
+            do
+            {
+                Console.Write("Логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Пароль: ");
+                string password = Console.ReadLine();
+                if (Task3.Validation(login, password) == false)
+                {
+                    Console.WriteLine("Введен неверный логин или пароль. У вас осталось {0} попытки(а).", 2 - i);
+                }
+                else
+                {
+                    result = true;
+                    Console.WriteLine("Добро пожаловать в систему, root!");
+                    break;
+                }
+                i++;
+            } while (i < 3 || result != true);
+
+            if (result == false) { Console.WriteLine("Доступ в систему заблокирован."); }
+            Console.ReadKey();
+        }
+    }
+
+    public class Task4
+    {
+        private int[,] arr;
+        private int maxVal;
+        private int minVal;
+
+
+        private Task4()
+        { }
+
+        public Task4(string pathFileWithValues)
+        {
+            string[] strInput = File.ReadAllLines(pathFileWithValues);
+
+            if (strInput.Length > 0)
+            {
+                int lengthColumns = strInput[0].Split(' ').Length;
+                int lengthRows = strInput.Length;
+                this.arr = new int[lengthColumns, lengthRows];
+
+                for (int i = 0; i < lengthColumns; i++)
+                {
+                    string[] currentRowArr = strInput[i].Split(' ');
+                    for (int j = 0; j < lengthRows; j++)
+                    {
+                        this.arr[i, j] = Convert.ToInt32(currentRowArr[j]);
+                    }
+                }
+            }
+        }
+
+        public Task4(int lengthColumns, int lengthRows)
+        {
+            this.arr = new int[lengthColumns, lengthRows];
+            Random rnd = new Random();
+            this.maxVal = 0;
+            this.minVal = 0;
+
+            for (int i = 0; i < lengthColumns; i++)
+            {
+                for (int j =  0; j < lengthRows; j++)
+                {
+                    this.arr[i, j] = rnd.Next(-100, 100);
+                    if (this.maxVal < this.arr[i, j]) { this.maxVal = this.arr[i, j]; }
+                    if (this.minVal > this.arr[i, j]) { this.minVal = this.arr[i, j]; }
+                }
+            }
+        }
+
+        public int Summ()
+        {
+            int summ = 0;
+            for (int i = 0; i < this.arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.arr.GetLength(1); j++)
+                {
+                    summ += this.arr[i, j];
+                }
+            }
+
+            return summ;
+        }
+
+        public int Summ(int startValue)
+        {
+            int summ = 0;
+            for (int i = 0; i < this.arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.arr.GetLength(1); j++)
+                {
+                    if (this.arr[i, j] > startValue)
+                    {
+                        summ += this.arr[i, j];
+                    }
+                }
+            }
+
+            return summ;
+        }
+
+        public int[] FindMaxValCoordinates()
+        {
+            int[] outputVal = new int[2];
+            for (int i = 0; i < this.arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.arr.GetLength(1); j++)
+                {
+                    if (this.maxVal == this.arr[i, j])
+                    {
+                        outputVal[0] = i;
+                        outputVal[1] = j;
+                    }
+                }
+            }
+            return outputVal;
+        }
+
+        public void ArrToFile(string outputFilePath)
+        {
+            string[] outputStr = new string[this.arr.GetLength(1)];
+            for (int i = 0; i < this.arr.GetLength(0); i++)
+            {
+                string tempStr = String.Empty;
+                for (int j = 0; j < this.arr.GetLength(1); j++)
+                {
+                    tempStr += this.arr[i, j] + " ";
+                }
+                outputStr[i] = tempStr;
+                tempStr = String.Empty;
+            }
+            File.WriteAllLines(outputFilePath, outputStr);
+        }
+
+        public int[,] GetArray()
+        {
+            return arr;
+        }
+
+        public int GetMin()
+        {
+            return minVal;
+        }
+
+        public int GetMax()
+        {
+            return maxVal;
+        }
+    }
+}
---
+++ Lecture4HomeWork/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Lecture4HomeWorkClasses;
+
+namespace Lecture4HomeWork
+{
+    class Task2UserInterface
+    {
+        public static void MainWindow()
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-2]";
+            Task2 t2;
+
+            Console.WriteLine("Уважаемый пользователь, вы хотите сгенерировать массив или загрузить его из файла?");
+            Console.WriteLine("\t1. Сгенерировать.");
+            Console.WriteLine("\t2. Загрузить из файла.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        t2 = GenerateArray();
+                        Actions(t2);
+                        break;
+                    case '2':
+                        t2 = LoadFromFile();
+                        Actions(t2);
+                        break;
+                }
+            }
+        }
+
+        private static void GetArrayOnScreen(Task2 t2)
+        {
+            int[] arr = t2.GetArray();
+            string arrStr = String.Empty;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrStr += arr[i] + " ";
+            }
+            Console.WriteLine(arrStr);
+            Console.ReadKey();
+        }
+
+        private static Task2 GenerateArray()
+        {
+            Console.Write("Введите размерность массива, начальное значение, а так же шаг приращения, через запятую: ");
+            string[] inputString = Console.ReadLine().Split(' ', ',').Where(x => x != "").ToArray(); 
+            Task2 t2 = new Task2(int.Parse(inputString[0]), int.Parse(inputString[1]), int.Parse(inputString[2]));
+
+            GetArrayOnScreen(t2);
+
+            return t2;
+        }
+
+        private static Task2 LoadFromFile()
+        {
+            Task2 t2 = new Task2(@"C:\arr.txt");
+
+            GetArrayOnScreen(t2);
+
+            return t2;
+        }
+
+        private static void Actions(Task2 t2)
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-5]";
+
+            Console.WriteLine("Уважаемый пользователь, какую операцию вы хотите проделать с массивом?");
+            Console.WriteLine("\t1. Вернуть сумму элементов массива.");
+            Console.WriteLine("\t2. Изменить знаки у всех элементов массива.");
+            Console.WriteLine("\t3. Умножить каждый элемент массива на заданное число.");
+            Console.WriteLine("\t4. Узнать максимальный элемент и количество таковых в массиве.");
+            Console.WriteLine("\t5. Записать массив в файл.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("Сумма элементов массива равна: " + t2.Summ());
+                        break;
+                    case '2':
+                        t2.Inverse();
+                        Console.WriteLine("Знаки элементов массива изменены!");
+                        GetArrayOnScreen(t2);
+                        break;
+                    case '3':
+                        Console.Write("Укажите на какое число необходимо умножить элементы массива?");
+                        t2.Multi(int.Parse(Console.ReadLine()));
+                        GetArrayOnScreen(t2);
+                        break;
+                    case '4':
+                        string outputStr = t2.MaxCount();
+                        Console.WriteLine(outputStr);
+                        Console.ReadKey();
+                        break;
+                    case '5':
+                        Console.Write("Укажите путь до файла, в который необходимо сохранить массив: ");
+                        t2.OutputToFile(Console.ReadLine());
+                        break;
+                }
+            }
+        }
+    }
+
+    class Task4UserInterface
+    {
+        public static void MainWindow()
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-2]";
+            Task4 t4;
+
+            Console.WriteLine("Уважаемый пользователь, вы хотите сгенерировать массив или загрузить его из файла?");
+            Console.WriteLine("\t1. Сгенерировать.");
+            Console.WriteLine("\t2. Загрузить из файла.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        t4 = GenerateArray();
+                        Actions(t4);
+                        break;
+                    case '2':
+                        t4 = LoadFromFile();
+                        Actions(t4);
+                        break;
+                }
+            }
+        }
+
+        private static void GetArrayOnScreen(Task4 t4)
+        {
+            int[,] arr = t4.GetArray();
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                string outputStr = String.Empty;
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    outputStr += arr[i, j] + " ";
+                }
+                Console.WriteLine(outputStr);
+            }
+            Console.ReadKey();
+        }
+
+        private static Task4 GenerateArray()
+        { 
+            Task4 t4;
+            Console.Write("Введите размерность массива по-горизонтали и по-вертикали, через запятую: ");
+            string[] inputVal = Console.ReadLine().Split(' ', ',').Where(x => x != "").ToArray();
+
+            t4 = new Task4(int.Parse(inputVal[0]), int.Parse(inputVal[1]));
+
+            GetArrayOnScreen(t4);
+
+            return t4;
+        }
+
+        private static Task4 LoadFromFile()
+        {
+            Task4 t4;
+            Console.Write("Укажите путь к файлу из которого необходимо загрузить массив: ");
+            t4 = new Task4(Console.ReadLine());
+
+            GetArrayOnScreen(t4);
+
+            return t4;
+        }
+
+        private static void Actions(Task4 t4)
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-2]";
+
+            Console.WriteLine("Уважаемый пользователь, какую операцию вы хотите проделать с массивом?");
+            Console.WriteLine("\t1. Вернуть сумму элементов массива.");
+            Console.WriteLine("\t2. Вернуть сумму элементов массива при значениях больше указанного.");
+            Console.WriteLine("\t3. Получить минимальный элемент массива.");
+            Console.WriteLine("\t4. Получить максимальный элемент массива.");
+            Console.WriteLine("\t5. Записать массив в файл.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("Сумма элементов массива равна: " + t4.Summ());
+                        break;
+                    case '2':
+                        Console.Write("Укажите значение при котором все элементы больше оного будут суммироваться: ");
+                        int summ = t4.Summ(int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Сумма значений при заданном условии: " + summ);
+                        Console.ReadKey();
+                        break;
+                    case '3':
+                        Console.Write("Минимальный элемент массива: " + t4.GetMin());
+                        Console.ReadKey();
+                        break;
+                    case '4':
+                        int[] outputValMaxCoord = t4.FindMaxValCoordinates();
+                        Console.Write("Максимальный элемент массива: " + t4.GetMax() + " и находится на позиции с координатами: x = " + outputValMaxCoord[0] + " y = " + outputValMaxCoord[1]);
+                        Console.ReadKey();
+                        break;
+                    case '5':
+                        Console.Write("Укажите путь до файла, в который необходимо сохранить массив: ");
+                        t4.ArrToFile(Console.ReadLine());
+                        break;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void TaskN1()
+        {
+            Task1 t1 = new Task1();
+            int[] arr = t1.GetArray();
+            Console.WriteLine("Созданный массив:");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Количество парных элементов с делителем 3 найдено: " + t1.FindPairValuesMod3());
+            Console.ReadKey();
+        }
+
+        public static void TaskN2()
+        {
+            Task2UserInterface.MainWindow();
+        }
+
+        public static void TaskN3()
+        {
+            Task3.LoginPasswordValidation();
+        }
+
+        public static void TaskN4()
+        {
+            Task4UserInterface.MainWindow();
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo selectedTask;
+            var rule = @"[1-4]";
+
+            Console.WriteLine("Добрый день, уважаемый пользователь. Демонстрацию работы какой программы вы бы хотели увидеть?");
+            Console.WriteLine("\t1. Программа нахождения пар чисел в линейном массиве.");
+            Console.WriteLine("\t2. Программа работы с одномерным массивом.");
+            Console.WriteLine("\t3. Программа проверки логинов/паролей с возможностью подгрузки оных из файла.");
+            Console.WriteLine("\t4. Программа работы с двумерным массивом.");
+            selectedTask = Console.ReadKey(true);
+            if (Regex.IsMatch(selectedTask.KeyChar.ToString(), rule))
+            {
+                switch (selectedTask.KeyChar)
+                {
+                    case '1':
+                        TaskN1();
+                        break;
+                    case '2':
+                        TaskN2();
+                        break;
+                    case '3':
+                        TaskN3();
+                        break;
+                    case '4':
+                        TaskN4();
+                        break;
+                }
+            }
+        }
+    }
+}
